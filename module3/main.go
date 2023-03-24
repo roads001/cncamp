82977dd1241487838f39649c3546ba2c78b1f81f
james roads <james@pemlex.de>
2023-03-24 18:17:05 +0100

    add metrics,files: main.go and httpserver-deployment.yaml

--- module3/main.go
+++ module3/main.go
@@ -2,18 +2,54 @@ package main
 
 import (
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
-var started = time.Now()
+var (
+	started          = time.Now()
+	requestDurations = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "httpserver",
+			Name:      "http_request_duration_seconds",
+			Help:      "A histogram of the HTTP request durations in seconds.",
+			//Buckets: []float64{0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
+			Buckets: prometheus.ExponentialBuckets(0.001, 2, 15),
+		}, []string{"step"})
+)
+
+type ExecutionTimer struct {
+	histo *prometheus.HistogramVec
+}
+
+func (et ExecutionTimer) Observe(value float64) {
+	(et.histo).WithLabelValues("total").Observe(value)
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("get", r.RequestURI)
+
+	// 启动一个计时器
+	exeTime := ExecutionTimer{
+		histo: requestDurations,
+	}
+	timer := prometheus.NewTimer(exeTime)
+	// 停止计时器
+	defer timer.ObserveDuration()
+
+	// sleep 0-2s
+	randInt := rand.Intn(2000)
+	// 将持续时间放进 requestDurations 的直方图指标中去
+	//exeTime.Observe(float64(randInt / 1000))
+	time.Sleep(time.Millisecond * time.Duration(randInt))
+	log.Println("sleep ", randInt)
 
 	//接收客户端 request，并将 request 中带的 header 写入 response header
 	for k, v := range r.Header { // return string, []string
@@ -61,11 +97,16 @@ func healthz10(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	prometheus.MustRegister(requestDurations)
+
 	mux := http.NewServeMux() //声明多路复用mux对象
 	// use mux.HandleFunc define routing
 	mux.HandleFunc("/", index)
 	mux.HandleFunc("/healthz", healthz)
 	mux.HandleFunc("/healthz10", healthz10)
+
+	// register a new handler for the /metrics endpoint
+	mux.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe("0.0.0.0:8080", mux)
 
